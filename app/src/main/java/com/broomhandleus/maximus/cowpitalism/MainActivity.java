1595d84715d8af0315ab9aac2a4d1b245ffe7f69
Connor Reeder <connor.reeder@ymail.com>
2017-05-25 15:27:13 -0400

    working

--- app/src/main/java/com/broomhandleus/maximus/cowpitalism/MainActivity.java
+++ app/src/main/java/com/broomhandleus/maximus/cowpitalism/MainActivity.java
@@ -2,12 +2,37 @@ package com.broomhandleus.maximus.cowpitalism;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button startButton;
+    Button rulesButton;
+    public static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startButton = (Button) findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Start button is clicked.");
+            }
+        });
+
+        rulesButton = (Button) findViewById(R.id.rulesButton);
+        rulesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Rules button is clicked.");
+            }
+        });
     }
+
+
 }

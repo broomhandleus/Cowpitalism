bed56c63154a78356922e90ee0e1187ef6b2aa12
Maxwell <maximusbroomhandle@gmail.com>
2017-05-25 16:14:58 -0400

    Added up navigation on rules activity

--- app/src/main/java/com/broomhandleus/maximus/cowpitalism/RulesActivity.java
+++ app/src/main/java/com/broomhandleus/maximus/cowpitalism/RulesActivity.java
@@ -2,14 +2,19 @@ package com.broomhandleus.maximus.cowpitalism;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class RulesActivity extends AppCompatActivity {
+
+    public static final String TAG = "RulesActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rules);
-
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 }
